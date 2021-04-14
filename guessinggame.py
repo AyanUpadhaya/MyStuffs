0fa95cdbb8f36223a94174be8807ad8922aa21c1
Ayan Upadhaya <ayanu881@gmail.com>
2021-04-14 23:35:48 +0600

    Modified the code to make the game a little hard

--- guessinggame.py
+++ guessinggame.py
@@ -1,15 +1,21 @@
 #lets import random lib
- 
-import random
- 
 #generate a random number for computer
- 
-computer=random.randint(1,20)
- 
+import random
+import time
+
+print("Enter your name:")
+name=input()
+print("Nice to meet you "+name)
+
+print("Computer:I have choosed a number between 1 to 20.")
+total_chance=3
+chance=0
+computer=random.randint(1,20) 
 guess=0
+time.sleep(3)
  
-while guess!=computer:
-         print("Guess a number between 1 to 20")
+while guess!=computer and chance!=total_chance:
+         print("Take a guess:")
          guess=int(input())
  
          if guess<computer:
@@ -18,3 +24,9 @@ while guess!=computer:
                   print("Too high")
          if guess==computer:
                   print("You Guessed it right")
+
+         chance=chance+1
+         if chance==total_chance:
+             print("Sorry you loose!")
+         else:
+             print("Left chances: "+str(total_chance-chance))
