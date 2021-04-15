d5362c215a8956d957bda752eed177dd8a1f9911
Ayan Upadhaya <ayanu881@gmail.com>
2021-04-16 01:58:35 +0600

    did some work to make the code look good

--- youtubebrowser.py
+++ youtubebrowser.py
@@ -1,34 +1,47 @@
-import webbrowser,os
+import webbrowser
 
 import datetime
-
-
-print("Enter your search:")
-
-keyword=input()
-
-search="https://www.youtube.com/results?search_query="+keyword
-
-print("Save history?")
-history=input()
 
 current_time=datetime.datetime.now()
 year=current_time.year
 month=current_time.month
 day=current_time.day
 hour=current_time.hour 
-min=current_time.minute 
-
+minute=current_time.minute 
 date=str(year)+"/"+str(month)+"/"+str(day)
-time=str(hour)+":"+str(min)
+time=str(hour)+":"+str(minute)
 
-if history=='y':
+def searchYoutube():
+	print("Enter your search:")
+
+	keyword=input()
+
+	search="https://www.youtube.com/results?search_query="+keyword
+
+	print("Save history?")
+	history=input()
+
+	if history=='y':
+		saveHistory(keyword,search)
+	else:
+		browser=webbrowser.open(search)
+
+
+def saveHistory(keyword,search):
 	browser=webbrowser.open(search)
 	with open("browsehistory.txt","a") as file:
 		file.write("Search title: "+keyword+"\n")
 		file.write(str(search)+"\n")
 		file.write(date+" "+time+"\n")
 		print("saved!")
-else:
-	browser=webbrowser.open(search)
+		print("Search again?")
+		newsearch=input()
+		if newsearch=='y':
+			searchYoutube()
+		else:
+			exit()
+
+
+searchYoutube()
+
 
